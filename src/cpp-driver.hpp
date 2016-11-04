50969e4496dc95485518c1649bc306f996537a85
Ivan Yakimov <ivan.yakimov.sibfu@yandex.ru>
2016-11-04 14:07:52 +0700

    Improve makefile, split Transformer and Engine sources
    Create two distinct directories for .cpp files related to Transformer and Engine, respectively.
    Makefile find, compile and link sources automatically - there is no need to write appropriate file names.

--- src/cpp-driver.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2016 Ivan Yakimov
-// Licensed under the Apache License, Version 2.0
-// e-mail: ivan.yakimov.research@yandex.ru
-
-#ifndef __CPP_DRIVER__
-#define __CPP_DRIVER__
-
-#endif
---
+++ src/trans/transformer_helper.hpp
@@ -0,0 +1,8 @@
+#ifndef __TRANSFORMER_HELPER__
+#define __TRANSFORMER_HELPER__
+namespace trans {
+  class TransformerHelper {
+    
+  };
+}
+#endif
